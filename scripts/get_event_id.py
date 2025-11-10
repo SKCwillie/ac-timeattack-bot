bc3b413a4d0957787588b837b05e6b0a8e06028b
skcwillie <rwilliams651@gmail.com>
2025-11-10 17:05:25 +0000

    refactoring how event Id is used

--- scripts/get_event_id.py
+++ scripts/get_event_id.py
@@ -1,12 +1,14 @@
 import os
 import json
+import pytz
 from dotenv import load_dotenv
 from datetime import datetime
-import pytz 
+from pathlib import Path
 
 
 load_dotenv("/home/ubuntu/ac-timeattack-bot/.env")
 SEASON_CONFIG_PATH = os.getenv("SEASON_CONFIG_PATH")
+EVENT_FILE = Path(os.getenv("EVENT_FILE"))
 
 def get_current_event_id():
     """Determine the current event based on CST time and seasonConfig.json."""
@@ -46,5 +48,27 @@ def get_current_event_id():
     return event_id
 
 
+def read_current_event():
+    """Read the current event ID from the file updated by event_watcher."""
+    if not EVENT_FILE.exists():
+        raise FileNotFoundError(
+            f"[get_event_id] currentEvent.json not found at {CURRENT_EVENT_PATH}. "
+            "Make sure the event_watcher service is running."
+        )
+
+    try:
+        with open(EVENT_FILE, "r") as f:
+            data = json.load(f)
+            event_id = data.get("event_id")
+            if not event_id:
+                raise ValueError("currentEvent.json missing 'event_id' key")
+            print(f'Current event from file: {event_id}')
+            return event_id
+    except json.JSONDecodeError:
+        raise ValueError(f"[get_event_id] currentEvent.json is not valid JSON.")
+
+
+
+
 if __name__ == "__main__":
-    get_current_event_id()
+    get_current_event_id() == read_current_event()
